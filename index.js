98638fa6bf83611ce6572c16192816e6f6014886
antoniomuso <antoniomusolino007@gmail.com>
2017-11-01 10:14:14 +0100

    compare

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-module.export = require('./build/Release/addon.node').levenshtein_distance
+var leven = require('./build/Release/addon.node').levenshtein_distance
 
+
+module.exports = (a, b) => {
+    if (a === b) {
+        return 0
+    }
+    return leven (a,b)
+}
