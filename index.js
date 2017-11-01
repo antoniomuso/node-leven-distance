c27ddbedd12f94e98f4a20161d58506b2f9ebf1f
Antonio Musolino <antoniomusolino007@gmail.com>
2017-11-01 11:20:52 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-
+var leven = require('./build/Release/addon.node').levenshtein_distance
 module.exports = (a, b) => {
     if (a === b) {
         return 0
     }
-    var leven = require('./build/Release/addon.node').levenshtein_distance
     return leven(a,b)
 }
